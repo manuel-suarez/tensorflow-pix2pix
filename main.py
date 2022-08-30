11830bb16cd82dde2bf6ada43ffb44f80486d2aa
Arturo Suárez <manuel.suarez@cimat.mx>
2022-08-30 15:41:46 -0500

    Image functions

--- main.py
+++ main.py
@@ -27,3 +27,54 @@ print(list(PATH.parent.iterdir()))
 sample_image = tf.io.read_file(str(PATH / 'train/1.jpg'))
 sample_image = tf.io.decode_jpeg(sample_image)
 print(sample_image.shape)
+
+# Image load
+def load(image_file):
+  # Read and decode an image file to a uint8 tensor
+  image = tf.io.read_file(image_file)
+  image = tf.io.decode_jpeg(image)
+
+  # Split each image tensor into two tensors:
+  # - one with a real building facade image
+  # - one with an architecture label image
+  w = tf.shape(image)[1]
+  w = w // 2
+  input_image = image[:, w:, :]
+  real_image = image[:, :w, :]
+
+  # Convert both images to float32 tensors
+  input_image = tf.cast(input_image, tf.float32)
+  real_image = tf.cast(real_image, tf.float32)
+
+  return input_image, real_image
+
+# The facade training set consist of 400 images
+BUFFER_SIZE = 400
+# The batch size of 1 produced better results for the U-Net in the original pix2pix experiment
+BATCH_SIZE = 1
+# Each image is 256x256 in size
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
+
+def resize(input_image, real_image, height, width):
+  input_image = tf.image.resize(input_image, [height, width],
+                                method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+  real_image = tf.image.resize(real_image, [height, width],
+                               method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+
+  return input_image, real_image
+
+def random_crop(input_image, real_image):
+  stacked_image = tf.stack([input_image, real_image], axis=0)
+  cropped_image = tf.image.random_crop(
+      stacked_image, size=[2, IMG_HEIGHT, IMG_WIDTH, 3])
+
+  return cropped_image[0], cropped_image[1]
+
+# Normalizing the images to [-1, 1]
+def normalize(input_image, real_image):
+  input_image = (input_image / 127.5) - 1
+  real_image = (real_image / 127.5) - 1
+
+  return input_image, real_image
+
