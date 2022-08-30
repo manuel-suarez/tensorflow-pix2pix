66352b3f6532e0840f8682bf45c57760e3006d74
Arturo Suárez <manuel.suarez@cimat.mx>
2022-08-30 15:54:15 -0500

    Define optimizers and function to save generated imgs

--- main.py
+++ main.py
@@ -278,3 +278,32 @@ def discriminator_loss(disc_real_output, disc_generated_output):
   total_disc_loss = real_loss + generated_loss
 
   return total_disc_loss
+
+# Optimizers
+generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                 discriminator_optimizer=discriminator_optimizer,
+                                 generator=generator,
+                                 discriminator=discriminator)
+
+def generate_images(model, test_input, tar, fname):
+  prediction = model(test_input, training=True)
+  plt.figure(figsize=(15, 15))
+
+  display_list = [test_input[0], tar[0], prediction[0]]
+  title = ['Input Image', 'Ground Truth', 'Predicted Image']
+
+  for i in range(3):
+    plt.subplot(1, 3, i+1)
+    plt.title(title[i])
+    # Getting the pixel values in the [0, 1] range to plot.
+    plt.imshow(display_list[i] * 0.5 + 0.5)
+    plt.axis('off')
+  plt.imsave(fname)
+
+for example_input, example_target in test_dataset.take(1):
+  generate_images(generator, example_input, example_target, 'example1.png')
